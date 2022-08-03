42e6a8dc3565a655bb29eae1b738fd70ff08be9a
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-03 13:57:36 +0200

    Legg til riktig loginservice url for local

--- src/frontend/Miljø.ts
+++ src/frontend/Miljø.ts
@@ -37,7 +37,7 @@ const Miljø = (): MiljøProps => {
     } else {
         return {
             soknadApi: `http://${window.location.hostname}:${window.location.port}${basePath}api`,
-            loginService: `http://${window.location.hostname}:8080/local/cookie?subject=12345678901`,
+            loginService: `http://${window.location.hostname}:8080/local/cookie?issuerId=selvbetjening&audience=aud-localhost&cookiename=localhost-idtoken&subject=12345678901`,
             visInnsendingsknapp: true,
             mellomlagerUrl: `http://${window.location.hostname}:8082/api/soknad/kontantstotte`, //todo
             modellVersjon: modellVersjon,
