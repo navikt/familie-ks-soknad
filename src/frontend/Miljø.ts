756dc7143924e4e218da12ca2ec27a85540d77a0
Daphne Leebeek <daphne.leebeek@nav.no>
2022-08-03 14:09:01 +0200

    Fjern todo på mellomlagerurl

--- src/frontend/Miljø.ts
+++ src/frontend/Miljø.ts
@@ -20,7 +20,7 @@ const Miljø = (): MiljøProps => {
             loginService: 'https://loginservice.dev.nav.no/login?',
             visInnsendingsknapp: true,
             mellomlagerUrl:
-                'https://familie-dokument.dev.nav.no/familie/dokument/api/soknad/kontantstotte', //todo
+                'https://familie-dokument.dev.nav.no/familie/dokument/api/soknad/kontantstotte',
             modellVersjon: modellVersjon,
             dokumentUrl:
                 'https://familie-dokument.dev.nav.no/familie/dokument/api/mapper/ANYTTHING', //Vil uansett gå til bucket "familievedlegg" enn så lenge
@@ -30,7 +30,7 @@ const Miljø = (): MiljøProps => {
             soknadApi: `https://www.nav.no${basePath}api`,
             loginService: 'https://loginservice.nav.no/login?',
             visInnsendingsknapp: false,
-            mellomlagerUrl: 'https://www.nav.no/familie/dokument/api/soknad/kontantstotte', //todo
+            mellomlagerUrl: 'https://www.nav.no/familie/dokument/api/soknad/kontantstotte',
             modellVersjon: modellVersjon,
             dokumentUrl: `https://www.nav.no/familie/dokument/api/mapper/ANYTTHING`, //Vil uansett gå til bucket "familievedlegg" enn så lenge,
         };
@@ -39,7 +39,7 @@ const Miljø = (): MiljøProps => {
             soknadApi: `http://${window.location.hostname}:${window.location.port}${basePath}api`,
             loginService: `http://${window.location.hostname}:8080/local/cookie?issuerId=selvbetjening&audience=aud-localhost&cookiename=localhost-idtoken&subject=12345678901`,
             visInnsendingsknapp: true,
-            mellomlagerUrl: `http://${window.location.hostname}:8082/api/soknad/kontantstotte`, //todo
+            mellomlagerUrl: `http://${window.location.hostname}:8082/api/soknad/kontantstotte`,
             modellVersjon: modellVersjon,
             dokumentUrl: `http://${window.location.hostname}:8082/api/mapper/ANYTTHING`,
         };
