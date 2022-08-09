140f028cb7e7a1ffd9d8b54c80390802e4d5559e
norafutsaeter <nora.futseter@nav.no>
2022-08-09 12:55:12 +0200

    Setter id

--- src/frontend/components/SøknadsSteg/OmDeg/spørsmål.ts
+++ src/frontend/components/SøknadsSteg/OmDeg/spørsmål.ts
@@ -8,7 +8,7 @@ export enum OmDegSpørsmålId {
     borPåRegistrertAdresse = 'bor-på-registrert-adresse',
     værtINorgeITolvMåneder = 'søker-vært-i-norge-sammenhengende-tolv-måneder',
     planleggerÅBoINorgeTolvMnd = 'søker-planlegger-å-bo-i-norge-sammenhengende-tolv-måneder',
-    yrkesaktivFemÅr = 'todo-søker-yrkesaktiv',
+    yrkesaktivFemÅr = 'søker-yrkesaktiv-fem-år',
 }
 
 export const omDegSpørsmålSpråkId: Record<OmDegSpørsmålId, string> = {
