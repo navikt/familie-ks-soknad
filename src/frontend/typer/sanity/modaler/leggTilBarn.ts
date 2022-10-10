6890ab49b24348180778382efb7d3a515b64754c
Daphne Leebeek <daphne.leebeek@nav.no>
2022-10-10 17:04:19 +0200

    Legg til sanity-tekster for legg til barn-modal

--- src/frontend/typer/sanity/modaler/leggTilBarn.ts
+++ src/frontend/typer/sanity/modaler/leggTilBarn.ts
@@ -1,6 +1,15 @@
 import { LocaleRecordBlock } from '../../common';
+import { ISanitySpørsmålDokument } from '../sanity';
 
 export interface ILeggTilBarnTekstinnhold {
+    tittel: LocaleRecordBlock;
     leggTilKnapp: LocaleRecordBlock;
     fjernKnapp: LocaleRecordBlock;
+    fornavn: ISanitySpørsmålDokument;
+    etternavn: ISanitySpørsmålDokument;
+    foedselsnummerEllerDNummer: ISanitySpørsmålDokument;
+    erBarnetFoedt: ISanitySpørsmålDokument;
+    ikkeFoedtAlert: LocaleRecordBlock;
+    barnetsNavnSubtittel: LocaleRecordBlock;
+    foedselsnummerAlert: LocaleRecordBlock;
 }
