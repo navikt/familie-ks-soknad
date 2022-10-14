1b72e62b91d06f0bdbd4ed900e0f1959b38d81f3
Daphne <daphne.leebeek@nav.no>
2022-10-14 14:05:50 +0200

    Legg ved alert dersom bruker huker av fosterbarn eller institusjon (#73)

--- src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.tsx
+++ src/frontend/components/SøknadsSteg/OmBarnaDine/OmBarnaDine.tsx
@@ -19,6 +19,7 @@ import Steg from '../../Felleskomponenter/Steg/Steg';
 import TekstBlock from '../../Felleskomponenter/TekstBlock';
 import { VedleggNotis } from '../../Felleskomponenter/VedleggNotis';
 import HvilkeBarnCheckboxGruppe from './HvilkeBarnCheckboxGruppe';
+import { IOmBarnaTekstinnhold } from './innholdTyper';
 import { OmBarnaDineSpørsmålId, omBarnaDineSpørsmålSpråkId } from './spørsmål';
 import { useOmBarnaDine } from './useOmBarnaDine';
 
@@ -34,9 +35,9 @@ const OmBarnaDine: React.FC = () => {
     const { søknad, tekster, plainTekst } = useApp();
     const { barnInkludertISøknaden } = søknad;
 
-    const {
-        [ESanitySteg.OM_BARNA]: { omBarnaTittel, hvemBarnehageplass },
-    } = tekster();
+    const teksterForSteg: IOmBarnaTekstinnhold = tekster()[ESanitySteg.OM_BARNA];
+    const { omBarnaTittel, hvemBarnehageplass, fosterbarn, institusjonKontantstoette } =
+        teksterForSteg;
 
     if (!barnInkludertISøknaden.length) {
         history.push('/velg-barn');
@@ -69,6 +70,11 @@ const OmBarnaDine: React.FC = () => {
                     nullstillValgteBarn={skjema.felter.erNoenAvBarnaFosterbarn.verdi === ESvar.NEI}
                     visFeilmelding={skjema.visFeilmeldinger}
                 />
+                {skjema.felter.erNoenAvBarnaFosterbarn.verdi === ESvar.JA && (
+                    <AlertStripe variant={'warning'}>
+                        <TekstBlock block={fosterbarn.alert} typografi={Typografi.BodyShort} />
+                    </AlertStripe>
+                )}
 
                 <JaNeiSpm
                     skjema={skjema}
@@ -84,7 +90,6 @@ const OmBarnaDine: React.FC = () => {
                         </AlertStripe>
                     }
                 />
-
                 <HvilkeBarnCheckboxGruppe
                     legendSpråkId={
                         omBarnaDineSpørsmålSpråkId[
@@ -98,6 +103,15 @@ const OmBarnaDine: React.FC = () => {
                     }
                     visFeilmelding={skjema.visFeilmeldinger}
                 />
+                {skjema.felter.oppholderBarnSegIInstitusjon.verdi === ESvar.JA && (
+                    <AlertStripe variant={'warning'}>
+                        <TekstBlock
+                            block={institusjonKontantstoette.alert}
+                            typografi={Typografi.BodyShort}
+                        />
+                    </AlertStripe>
+                )}
+
                 <JaNeiSpm
                     skjema={skjema}
                     felt={skjema.felter.erBarnAdoptertFraUtland}
