b183d59246add3d4c01bf0a198750feb9d6412e9
halvormundal <halvorbmundal@gmail.com>
2022-10-17 12:00:47 +0200

    Oppdater test til å teste ved kun to elementer også

--- src/frontend/utils/slåSammen.test.ts
+++ src/frontend/utils/slåSammen.test.ts
@@ -9,10 +9,12 @@ describe('slåSammen', () => {
         expect(slåSammen(['test'])).toEqual('test');
     });
 
-    test('Returnerer kommaseparert streng med og på slutten ved flere elementer', () => {
-        const sammenslåttTekst1 = slåSammen(['test1', 'test2', 'test3']);
-        expect(sammenslåttTekst1).toEqual('test1, test2 og test3');
+    test('Returnerer "element1 og element2" ved to elementer ', () => {
+        const sammenslåttTekst1 = slåSammen(['test1', 'test2']);
+        expect(sammenslåttTekst1).toEqual('test1 og test2');
+    });
 
+    test('Returnerer kommaseparert streng med "og" på slutten ved flere elementer', () => {
         const sammenslåttTekst2 = slåSammen(['test1', 'test2', 'test3', 'test4', 'test5', 'test6']);
         expect(sammenslåttTekst2).toEqual('test1, test2, test3, test4, test5 og test6');
     });
