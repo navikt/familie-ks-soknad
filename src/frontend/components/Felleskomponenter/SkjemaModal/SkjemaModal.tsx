4250feb15122d5eb46543ebd3cb44d89175a202c
halvorbmundal <halvorbmundal@users.noreply.github.com>
2022-11-25 09:22:05 +0100

    Fjern avviklede felter (#109)
    * Fjern deprecatede felter fra SkjemaCheckbox
    
    * Fjern avviklede felter fra SkjemaModal og VedleggNotis
    
    * Fjern avvilkede felter fra SkjemaFieldset
    
    * Fjern avvilkede felter fra SkjemaFeltInput
    
    * Fjern avvilkede felter fra JaNeiSpørsmål
    
    * Fjern avvilkede felter fra PeriodeOppsummering

--- src/frontend/components/Felleskomponenter/SkjemaModal/SkjemaModal.tsx
+++ src/frontend/components/Felleskomponenter/SkjemaModal/SkjemaModal.tsx
@@ -2,15 +2,12 @@ import React, { ReactNode } from 'react';
 
 import styled from 'styled-components';
 
-import { Innholdstittel } from 'nav-frontend-typografi';
-
 import { Button, Modal } from '@navikt/ds-react';
 
 import { useApp } from '../../../context/AppContext';
 import { LocaleRecordBlock, Typografi } from '../../../typer/common';
 import { FlettefeltVerdier } from '../../../typer/kontrakt/generelle';
 import ModalContent from '../ModalContent';
-import SpråkTekst from '../SpråkTekst/SpråkTekst';
 import TekstBlock from '../TekstBlock';
 
 const StyledButton = styled(Button)`
@@ -21,33 +18,23 @@ const StyledButton = styled(Button)`
         box-sizing: border-box;
     }
 `;
-const StyledInnholdstittel = styled(Innholdstittel)`
-    text-align: center;
-    padding: 2rem 0;
-`;
 
 const SkjemaModal: React.FC<{
     erÅpen: boolean;
     toggleModal: () => void;
-    /** @deprecated **/
-    modalTittelSpråkId?: string; //todo: fjerne når vi har gått over til sanity
     submitSpinner?: boolean;
     valideringErOk: () => boolean;
     onAvbrytCallback?: () => void;
-    /** @deprecated **/
-    submitKnappSpråkId?: string; //todo: fjerne når vi har gått over til sanity
     onSubmitCallback: () => void;
-    tittel?: LocaleRecordBlock;
-    submitKnappTekst?: ReactNode;
+    tittel: LocaleRecordBlock;
+    submitKnappTekst: ReactNode;
     flettefelter?: FlettefeltVerdier;
 }> = ({
     erÅpen,
     toggleModal,
-    modalTittelSpråkId,
     submitSpinner = false,
     valideringErOk,
     onAvbrytCallback,
-    submitKnappSpråkId,
     onSubmitCallback,
     tittel,
     submitKnappTekst,
@@ -65,11 +52,6 @@ const SkjemaModal: React.FC<{
             aria-label={plainTekst(tittel, flettefelter)}
         >
             <ModalContent>
-                {modalTittelSpråkId && (
-                    <StyledInnholdstittel>
-                        <SpråkTekst id={modalTittelSpråkId} />
-                    </StyledInnholdstittel>
-                )}
                 {tittel && (
                     <TekstBlock
                         block={tittel}
@@ -89,7 +71,6 @@ const SkjemaModal: React.FC<{
                             onSubmitCallback();
                         }}
                     >
-                        {submitKnappSpråkId && <SpråkTekst id={submitKnappSpråkId} />}
                         {submitKnappTekst}
                     </StyledButton>
                 </form>
