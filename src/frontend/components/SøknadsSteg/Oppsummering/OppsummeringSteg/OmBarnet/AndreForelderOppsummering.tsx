4d14b73254348234a691bc9b7a03a62efb4ba8b5
Daphne Leebeek <daphne.leebeek@nav.no>
2022-12-13 12:22:15 +0100

    Fiks bugs og gammelt design for spørsmål om barn 2 har samme forelder som barn 1

--- src/frontend/components/SøknadsSteg/Oppsummering/OppsummeringSteg/OmBarnet/AndreForelderOppsummering.tsx
+++ src/frontend/components/SøknadsSteg/Oppsummering/OppsummeringSteg/OmBarnet/AndreForelderOppsummering.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { BodyShort } from '@navikt/ds-react';
 import { ESvar } from '@navikt/familie-form-elements';
 
 import { useApp } from '../../../../../context/AppContext';
@@ -29,99 +30,116 @@ const AndreForelderOppsummering: React.FC<{
     const flettefelter = { barnetsNavn: barn.navn };
     return (
         <>
-            <StyledOppsummeringsFeltGruppe>
-                {andreForelder[andreForelderDataKeySpørsmål.navn].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={omBarnetTekster.barnetsAndreForelder}
-                        søknadsvar={
-                            andreForelder[andreForelderDataKeySpørsmål.navn].svar !==
-                            AlternativtSvarForInput.UKJENT
-                                ? andreForelder[andreForelderDataKeySpørsmål.navn].svar
-                                : plainTekst(omBarnetTekster.navnAndreForelder.checkboxLabel)
-                        }
-                    />
-                )}
-                {andreForelder[andreForelderDataKeySpørsmål.fnr].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={omBarnetTekster.foedselsnummerDnummerAndreForelder.sporsmal}
-                        søknadsvar={
-                            andreForelder[andreForelderDataKeySpørsmål.fnr].svar !==
-                            AlternativtSvarForInput.UKJENT
-                                ? andreForelder[andreForelderDataKeySpørsmål.fnr].svar
-                                : plainTekst(
-                                      omBarnetTekster.foedselsnummerDnummerAndreForelder
-                                          .checkboxLabel
-                                  )
-                        }
-                    />
-                )}
-                {andreForelder[andreForelderDataKeySpørsmål.fødselsdato].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={omBarnetTekster.foedselsdatoAndreForelder.sporsmal}
-                        søknadsvar={formaterDatoMedUkjent(
-                            andreForelder[andreForelderDataKeySpørsmål.fødselsdato].svar,
-                            plainTekst(omBarnetTekster.foedselsdatoAndreForelder.checkboxLabel)
-                        )}
-                    />
-                )}
-                {andreForelder[andreForelderDataKeySpørsmål.yrkesaktivFemÅr].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={omBarnetTekster.medlemAvFolktetrygdenAndreForelder.sporsmal}
-                        flettefelter={flettefelter}
-                        søknadsvar={
-                            andreForelder[andreForelderDataKeySpørsmål.yrkesaktivFemÅr].svar
-                        }
-                    />
-                )}
-                {andreForelder[andreForelderDataKeySpørsmål.arbeidUtlandet].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={
-                            barn.andreForelderErDød.svar === ESvar.JA
-                                ? omBarnetTekster.arbeidUtenforNorgeAndreForelderGjenlevende
-                                      .sporsmal
-                                : omBarnetTekster.arbeidUtenforNorgeAndreForelder.sporsmal
-                        }
-                        flettefelter={flettefelter}
-                        søknadsvar={andreForelder[andreForelderDataKeySpørsmål.arbeidUtlandet].svar}
-                    />
-                )}
+            {andreForelder[andreForelderDataKeySpørsmål.kanIkkeGiOpplysninger].svar ===
+            ESvar.NEI ? (
+                <StyledOppsummeringsFeltGruppe>
+                    {andreForelder[andreForelderDataKeySpørsmål.navn].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={omBarnetTekster.navnAndreForelder.sporsmal}
+                            søknadsvar={
+                                andreForelder[andreForelderDataKeySpørsmål.navn].svar !==
+                                AlternativtSvarForInput.UKJENT
+                                    ? andreForelder[andreForelderDataKeySpørsmål.navn].svar
+                                    : plainTekst(omBarnetTekster.navnAndreForelder.checkboxLabel)
+                            }
+                        />
+                    )}
+                    {andreForelder[andreForelderDataKeySpørsmål.fnr].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={
+                                omBarnetTekster.foedselsnummerDnummerAndreForelder.sporsmal
+                            }
+                            søknadsvar={
+                                andreForelder[andreForelderDataKeySpørsmål.fnr].svar !==
+                                AlternativtSvarForInput.UKJENT
+                                    ? andreForelder[andreForelderDataKeySpørsmål.fnr].svar
+                                    : plainTekst(
+                                          omBarnetTekster.foedselsnummerDnummerAndreForelder
+                                              .checkboxLabel
+                                      )
+                            }
+                        />
+                    )}
+                    {andreForelder[andreForelderDataKeySpørsmål.fødselsdato].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={omBarnetTekster.foedselsdatoAndreForelder.sporsmal}
+                            søknadsvar={formaterDatoMedUkjent(
+                                andreForelder[andreForelderDataKeySpørsmål.fødselsdato].svar,
+                                plainTekst(omBarnetTekster.foedselsdatoAndreForelder.checkboxLabel)
+                            )}
+                        />
+                    )}
+                    {andreForelder[andreForelderDataKeySpørsmål.yrkesaktivFemÅr].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={
+                                omBarnetTekster.medlemAvFolktetrygdenAndreForelder.sporsmal
+                            }
+                            flettefelter={flettefelter}
+                            søknadsvar={
+                                andreForelder[andreForelderDataKeySpørsmål.yrkesaktivFemÅr].svar
+                            }
+                        />
+                    )}
+                    {andreForelder[andreForelderDataKeySpørsmål.arbeidUtlandet].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={
+                                barn.andreForelderErDød.svar === ESvar.JA
+                                    ? omBarnetTekster.arbeidUtenforNorgeAndreForelderGjenlevende
+                                          .sporsmal
+                                    : omBarnetTekster.arbeidUtenforNorgeAndreForelder.sporsmal
+                            }
+                            flettefelter={flettefelter}
+                            søknadsvar={
+                                andreForelder[andreForelderDataKeySpørsmål.arbeidUtlandet].svar
+                            }
+                        />
+                    )}
 
-                {andreForelder.arbeidsperioderUtland.map((periode, index) => (
-                    <ArbeidsperiodeOppsummering
-                        key={`arbeidsperiode-${index}`}
-                        nummer={index + 1}
-                        arbeidsperiode={periode}
-                        gjelderUtlandet={true}
-                        personType={PersonType.andreForelder}
-                        erDød={barn[barnDataKeySpørsmål.andreForelderErDød].svar === ESvar.JA}
-                        barn={barn}
-                    />
-                ))}
+                    {andreForelder.arbeidsperioderUtland.map((periode, index) => (
+                        <ArbeidsperiodeOppsummering
+                            key={`arbeidsperiode-${index}`}
+                            nummer={index + 1}
+                            arbeidsperiode={periode}
+                            gjelderUtlandet={true}
+                            personType={PersonType.andreForelder}
+                            erDød={barn[barnDataKeySpørsmål.andreForelderErDød].svar === ESvar.JA}
+                            barn={barn}
+                        />
+                    ))}
 
-                {andreForelder[andreForelderDataKeySpørsmål.pensjonUtland].svar && (
-                    <OppsummeringFelt
-                        spørsmålstekst={
-                            barn.andreForelderErDød.svar === ESvar.JA
-                                ? omBarnetTekster.pensjonUtlandAndreForelderGjenlevende.sporsmal
-                                : omBarnetTekster.pensjonUtlandAndreForelder.sporsmal
-                        }
-                        flettefelter={flettefelter}
-                        søknadsvar={andreForelder[andreForelderDataKeySpørsmål.pensjonUtland].svar}
-                    />
-                )}
+                    {andreForelder[andreForelderDataKeySpørsmål.pensjonUtland].svar && (
+                        <OppsummeringFelt
+                            spørsmålstekst={
+                                barn.andreForelderErDød.svar === ESvar.JA
+                                    ? omBarnetTekster.pensjonUtlandAndreForelderGjenlevende.sporsmal
+                                    : omBarnetTekster.pensjonUtlandAndreForelder.sporsmal
+                            }
+                            flettefelter={flettefelter}
+                            søknadsvar={
+                                andreForelder[andreForelderDataKeySpørsmål.pensjonUtland].svar
+                            }
+                        />
+                    )}
 
-                {andreForelder.pensjonsperioderUtland.map((periode, index) => (
-                    <PensjonsperiodeOppsummering
-                        key={`pensjonsperiode-utland-andre-forelder${index}`}
-                        nummer={index + 1}
-                        pensjonsperiode={periode}
-                        gjelderUtlandet={true}
-                        personType={PersonType.andreForelder}
-                        erDød={barn[barnDataKeySpørsmål.andreForelderErDød].svar === ESvar.JA}
-                        barn={barn}
-                    />
-                ))}
-            </StyledOppsummeringsFeltGruppe>
+                    {andreForelder.pensjonsperioderUtland.map((periode, index) => (
+                        <PensjonsperiodeOppsummering
+                            key={`pensjonsperiode-utland-andre-forelder${index}`}
+                            nummer={index + 1}
+                            pensjonsperiode={periode}
+                            gjelderUtlandet={true}
+                            personType={PersonType.andreForelder}
+                            erDød={barn[barnDataKeySpørsmål.andreForelderErDød].svar === ESvar.JA}
+                            barn={barn}
+                        />
+                    ))}
+                </StyledOppsummeringsFeltGruppe>
+            ) : (
+                <StyledOppsummeringsFeltGruppe>
+                    <BodyShort>
+                        {plainTekst(omBarnetTekster.navnAndreForelder.checkboxLabel)}
+                    </BodyShort>
+                </StyledOppsummeringsFeltGruppe>
+            )}
         </>
     );
 };
