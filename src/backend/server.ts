6d0f954d472d8ee04215bf0c65419c4e2f800406
Daphne Leebeek <daphne.leebeek@nav.no>
2023-01-11 13:21:59 +0100

    Legg til nav.no og familie-dokument til csp-regler

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -53,7 +53,7 @@ app.use((_req, res, next) => {
             'static.hotjar.com',
         ].join(
             ' '
-        )}; connect-src 'self' api.amplitude.com amplitude.nav.no nav.psplugin.com tjenester.nav.no vc.hotjar.io by26nl8j.apicdn.sanity.io;`
+        )}; connect-src 'self' api.amplitude.com amplitude.nav.no nav.psplugin.com tjenester.nav.no vc.hotjar.io by26nl8j.apicdn.sanity.io www.nav.no familie-dokument.dev.nav.no;`
     );
     next();
 });
