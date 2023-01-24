a37997a79f380c4960a7ae0c46256e288cc305fe
bragejahren <brage.ekroll.jahren@nav.no>
2023-01-24 12:21:04 +0100

    Justert routes/api.ts ytterligere

--- src/backend/routes/api.ts
+++ src/backend/routes/api.ts
@@ -1,7 +1,6 @@
 import express, { Express, RequestHandler } from 'express';
 
-import Miljø from '../../shared-utils/Miljø';
-import { basePath } from '../environment';
+import Miljø, { basePath } from '../../shared-utils/Miljø';
 import { erklaeringInterceptor } from '../middlewares/erklaering-interceptor';
 import { escapeBody } from '../middlewares/escape';
 import { modellVersjonInterceptor } from '../middlewares/modell-versjon-interceptor';
@@ -15,17 +14,17 @@ export const konfigurerApi = (app: Express): Express => {
     app.use(`${basePath}api/soknad`, erklaeringInterceptor);
     app.use(`${basePath}api/soknad`, escapeBody);
     app.use(
-        `${process.env.BASE_PATH}/api`,
+        `${basePath}api`,
         addCallId(),
         attachToken('familie-baks-soknad-api'),
-        createApiForwardingFunction(Miljø().soknadApiUrl, `${process.env.BASE_PATH}api`)
+        createApiForwardingFunction(Miljø().soknadApiUrl, `${basePath}api`)
     );
 
     app.use(
-        `${process.env.BASE_PATH}/dokument`,
+        `${basePath}dokument`,
         addCallId(),
         attachToken('familie-dokument'),
-        createApiForwardingFunction(Miljø().dokumentUrl, `${process.env.BASE_PATH}dokument`)
+        createApiForwardingFunction(Miljø().dokumentUrl, `${basePath}dokument`)
     );
 
     return app;
