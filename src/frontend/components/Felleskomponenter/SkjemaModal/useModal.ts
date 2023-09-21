ef1ddf023a7406cc18dba74bd051032aea13d79b
Ida Merete Enholm <ida.merete.enholm@nav.no>
2023-09-21 18:50:36 +0200

    Fjern toggleModal-funksjon som ikke brukes lenger

--- src/frontend/components/Felleskomponenter/SkjemaModal/useModal.ts
+++ src/frontend/components/Felleskomponenter/SkjemaModal/useModal.ts
@@ -2,16 +2,13 @@ import { useState } from 'react';
 
 const useModal = () => {
     const [modalÅpen, settModalÅpen] = useState<boolean>(false);
-    const toggleModal = () => {
-        settModalÅpen(!modalÅpen);
-    };
     const lukkModal = () => {
         settModalÅpen(false);
     };
     const åpneModal = () => {
         settModalÅpen(true);
     };
-    return { erÅpen: modalÅpen, toggleModal, lukkModal, åpneModal };
+    return { erÅpen: modalÅpen, lukkModal, åpneModal };
 };
 
 export default useModal;
