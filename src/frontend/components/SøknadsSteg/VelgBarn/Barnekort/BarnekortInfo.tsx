91f82decb4d75a7753ef54bb4887e9ba28e297e3
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-07-16 13:57:46 +0200

    Legger til komponenter som brukes av BarneKort

---
+++ src/frontend/components/SøknadsSteg/VelgBarn/Barnekort/BarnekortInfo.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react';
+
+import { Box, Label, BodyShort } from '@navikt/ds-react';
+
+export const BarnekortInfo: React.FC<{ label: ReactNode; verdi: ReactNode }> = ({
+    label,
+    verdi,
+}) => {
+    return (
+        <Box>
+            <Label as="p">{label}</Label>
+            <BodyShort>{verdi}</BodyShort>
+        </Box>
+    );
+};
