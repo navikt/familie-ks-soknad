635d861f8dd7f39bbc734d68dfc7bde1eb528911
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-07-16 12:04:20 +0200

    Fjerner unøvendig guide = undefined

--- src/frontend/components/Felleskomponenter/Steg/Steg.tsx
+++ src/frontend/components/Felleskomponenter/Steg/Steg.tsx
@@ -90,7 +90,7 @@ const StepperContainer = styled.div<{ $antallSteg: number }>`
       `}
 }`;
 
-function Steg({ tittel, guide = undefined, skjema, gåVidereCallback, children }: ISteg) {
+function Steg({ tittel, guide, skjema, gåVidereCallback, children }: ISteg) {
     const navigate = useNavigate();
     const { erÅpen: erModellVersjonModalÅpen, åpneModal: åpneModellVersjonModal } = useModal();
     const {
