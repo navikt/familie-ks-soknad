122bf7341e89c3d49c0cdb8c4529fd0f53fc22de
Hanne Olsen <hanne.olsen@nav.no>
2024-08-15 13:30:17 +0200

    Logger eksplisitt til Kibana i tillegg til Sentry når nettverksrequesten skal feile 'mykt' og ikke kaste feil

--- src/frontend/hooks/useSendInnSkjema.tsx
+++ src/frontend/hooks/useSendInnSkjema.tsx
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/react';
 import { AxiosError } from 'axios';
 
+import { logError, logSecure } from '@navikt/familie-logging';
 import { Ressurs, RessursStatus } from '@navikt/familie-typer';
 
 import Miljø from '../../shared-utils/Miljø';
@@ -49,6 +50,8 @@ export const useSendInnSkjema = (): {
                         Sentry.captureException(
                             new Error('Klarte ikke sende inn søknaden', { cause: res.message })
                         );
+                        logError('Klarte ikke sende inn søknaden, se i securelog');
+                        logSecure('Klarte ikke sende inn søknaden', { formatert, res });
                     }
                 }
             );
