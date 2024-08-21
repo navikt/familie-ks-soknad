53b14d6f89508f98a337ddc4833f82e21c9d93ad
Hanne Olsen <hanne.olsen@nav.no>
2024-08-21 09:24:33 +0200

    Legger til varslinger søknadskontrakt og kall for å sende inn søknad (#612)
    * Legger til varslinger på opprettelse av søknadskontrakt og feilende kall for å sende søknaden
    
    * Bedre variabelnavn
    
    Co-authored-by: Thomas Alm <106926075+thoalm@users.noreply.github.com>
    
    * Logger eksplisitt til Kibana i tillegg til Sentry når nettverksrequesten skal feile 'mykt' og ikke kaste feil
    
    * Fjerner backend-pakke som ikke kan inkluderes i frontend
    
    ---------
    
    Co-authored-by: Thomas Alm <106926075+thoalm@users.noreply.github.com>

--- src/frontend/hooks/useSendInnSkjema.tsx
+++ src/frontend/hooks/useSendInnSkjema.tsx
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/react';
 import { AxiosError } from 'axios';
 
 import { Ressurs, RessursStatus } from '@navikt/familie-typer';
@@ -27,28 +28,37 @@ export const useSendInnSkjema = (): {
     const sendInnSkjema = async (): Promise<[boolean, ISøknadKontrakt]> => {
         settInnsendingStatus({ status: RessursStatus.HENTER });
 
-        const formatert: ISøknadKontrakt = dataISøknadKontraktFormat(
-            valgtLocale,
-            søknad,
-            tekster(),
-            tilRestLocaleRecord
-        );
+        try {
+            const formatert: ISøknadKontrakt = dataISøknadKontraktFormat(
+                valgtLocale,
+                søknad,
+                tekster(),
+                tilRestLocaleRecord
+            );
 
-        const res = await sendInn<ISøknadKontrakt>(
-            formatert,
-            axiosRequest,
-            `${soknadApiProxyUrl}/soknad/kontantstotte/v4`,
-            (res: AxiosError) => {
-                const responseData = res.response?.data as Ressurs<IKvittering>;
-                if (responseData && erModellMismatchResponsRessurs(responseData)) {
-                    settSisteModellVersjon(responseData.data.modellVersjon);
+            const res = await sendInn<ISøknadKontrakt>(
+                formatert,
+                axiosRequest,
+                `${soknadApiProxyUrl}/soknad/kontantstotte/v4`,
+                (res: AxiosError) => {
+                    const responseData = res.response?.data as Ressurs<IKvittering>;
+                    if (responseData && erModellMismatchResponsRessurs(responseData)) {
+                        settSisteModellVersjon(responseData.data.modellVersjon);
+                    } else {
+                        //Denne skal feile mykt, med en custom feilmelding til brukeren. Kaster dermed ingen feil her.
+                        Sentry.captureException(
+                            new Error('Klarte ikke sende inn søknaden', { cause: res.message })
+                        );
+                    }
                 }
-            }
-        );
+            );
 
-        settInnsendingStatus(res);
+            settInnsendingStatus(res);
 
-        return [res.status === RessursStatus.SUKSESS, formatert];
+            return [res.status === RessursStatus.SUKSESS, formatert];
+        } catch (error) {
+            throw new Error('Søknaden feilet på innsending', { cause: error });
+        }
     };
 
     return {
