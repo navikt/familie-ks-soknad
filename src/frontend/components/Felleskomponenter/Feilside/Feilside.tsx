5b967a468ab740470ce4bc7b505a43d8d00f857e
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2024-08-22 15:08:18 +0200

    Legger til ny feilside

--- src/frontend/components/Felleskomponenter/Feilside/Feilside.tsx
+++ src/frontend/components/Felleskomponenter/Feilside/Feilside.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { FC, useEffect } from 'react';
 
-import { Alert, BodyShort } from '@navikt/ds-react';
+import { Box } from '@navikt/ds-react';
+import { setAvailableLanguages } from '@navikt/nav-dekoratoren-moduler';
 
-import { useSpråk } from '../../../context/SpråkContext';
-import { LocaleType } from '../../../typer/common';
+import InnholdContainer from '../InnholdContainer/InnholdContainer';
 
-export const Feilside: React.FC = () => {
-    const { valgtLocale } = useSpråk();
+import { FeilsideHeading } from './FeilsideHeading';
+import { FeilsideInnhold } from './FeilsideInnhold';
+import { FeilsideStatuskode } from './FeilsideStatuskode';
 
-    const feilsidetekstPåRiktigSpråk = () => {
-        switch (valgtLocale) {
-            case LocaleType.nb:
-                return 'En feil har oppstått! Vennligst prøv igjen. Hvis det fremdeles er problem må du bruke PDF/papir-skjema for å søke.';
-            case LocaleType.nn:
-                return 'Ein feil har oppstått! Ver venleg og prøv igjen. Dersom det framleis er problem må du bruke PDF/papir-skjema for å søke.';
-            case LocaleType.en:
-                return 'An error has occurred. Please try again. If the problem persists, you will need to apply using a PDF/paper application form.';
-        }
+export const Feilside: FC = () => {
+    useEffect(() => {
+        visSpråkvelger();
+    }, []);
+
+    const visSpråkvelger = () => {
+        setAvailableLanguages([
+            { locale: 'nb', handleInApp: true },
+            { locale: 'nn', handleInApp: true },
+            { locale: 'en', handleInApp: true },
+        ]).then();
     };
 
+    // TODO: Er det mulig å få andre statuskoder enn 500? Hvis ja, hvordan finner vi dem og viser det her på feilsiden?
+    const statuskode = '500';
+
     return (
-        <div>
-            <Alert variant="error" inline aria-live={'polite'}>
-                <BodyShort>{feilsidetekstPåRiktigSpråk()}</BodyShort>
-            </Alert>
-        </div>
+        <InnholdContainer>
+            <Box marginBlock="32">
+                <FeilsideStatuskode statuskode={statuskode} />
+                <FeilsideHeading />
+                <FeilsideInnhold />
+            </Box>
+        </InnholdContainer>
     );
 };
---
+++ src/frontend/components/Felleskomponenter/Feilside/FeilsideInnhold.tsx
@@ -0,0 +1,101 @@
+import React, { FC } from 'react';
+
+import { BodyShort, Link, List } from '@navikt/ds-react';
+
+import { useSpråk } from '../../../context/SpråkContext';
+import { LocaleType } from '../../../typer/common';
+
+import { GåTilBakeTilForrigeSideLenke } from './GåTilbakeTilForrigeSideLenke';
+import { LastInnSidenPåNyttLenke } from './LastInnSidenPåNyttLenke';
+
+export const FeilsideInnhold: FC = () => {
+    const { valgtLocale } = useSpråk();
+
+    return (
+        <>
+            {valgtLocale === LocaleType.nb && (
+                <>
+                    <BodyShort spacing>
+                        En teknisk feil på våre servere gjør at siden er utilgjengelig. Dette
+                        skyldes ikke noe du gjorde.
+                    </BodyShort>
+                    <BodyShort>Du kan prøve å</BodyShort>
+                    <List>
+                        <List.Item>
+                            vente noen minutter og{' '}
+                            <LastInnSidenPåNyttLenke>laste siden på nytt</LastInnSidenPåNyttLenke>
+                        </List.Item>
+                        <List.Item>
+                            <GåTilBakeTilForrigeSideLenke>
+                                gå tilbake til forrige side
+                            </GåTilBakeTilForrigeSideLenke>
+                        </List.Item>
+                    </List>
+                    <BodyShort>
+                        Hvis problemet vedvarer, kan du{' '}
+                        <Link href="https://nav.no/kontaktoss" target="_blank">
+                            kontakte oss (åpnes i ny fane)
+                        </Link>
+                        .
+                    </BodyShort>
+                </>
+            )}
+
+            {valgtLocale === LocaleType.nn && (
+                <>
+                    <BodyShort spacing>
+                        Ein teknisk feil på våre servere gjer at sida er utilgjengeleg. Dette
+                        skuldast ikkje noko du gjorde.
+                    </BodyShort>
+                    <BodyShort>Du kan prøve å</BodyShort>
+                    <List>
+                        <List.Item>
+                            vente nokre minutt og{' '}
+                            <LastInnSidenPåNyttLenke>laste sida på nytt</LastInnSidenPåNyttLenke>
+                        </List.Item>
+                        <List.Item>
+                            <GåTilBakeTilForrigeSideLenke>
+                                gå tilbake til førre side
+                            </GåTilBakeTilForrigeSideLenke>
+                        </List.Item>
+                    </List>
+                    <BodyShort>
+                        Viss problemet held fram, kan du{' '}
+                        <Link href="https://nav.no/kontaktoss" target="_blank">
+                            kontakte oss (opnast i ny fane)
+                        </Link>
+                        .
+                    </BodyShort>
+                </>
+            )}
+
+            {valgtLocale === LocaleType.en && (
+                <>
+                    <BodyShort spacing>
+                        A technical error on our servers has made the page unavailable. This is not
+                        your fault.
+                    </BodyShort>
+                    <BodyShort>You can try to</BodyShort>
+                    <List>
+                        <List.Item>
+                            wait a few minutes and{' '}
+                            <LastInnSidenPåNyttLenke>reload the page</LastInnSidenPåNyttLenke>
+                        </List.Item>
+                        <List.Item>
+                            <GåTilBakeTilForrigeSideLenke>
+                                go back to the previous page
+                            </GåTilBakeTilForrigeSideLenke>
+                        </List.Item>
+                    </List>
+                    <BodyShort>
+                        If the problem persists, you can{' '}
+                        <Link href="https://nav.no/kontaktoss" target="_blank">
+                            contact us (opens in a new tab)
+                        </Link>
+                        .
+                    </BodyShort>
+                </>
+            )}
+        </>
+    );
+};
