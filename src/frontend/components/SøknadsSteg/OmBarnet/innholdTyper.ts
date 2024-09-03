549729ee5460793fc3c32ff1930e76c60b645459
Ole Kristian Rudjord <89088327+ole-kristian-rudjord@users.noreply.github.com>
2024-09-03 09:09:13 +0200

    Legger til guide på alle steg (#626)
    * Legger til guide for 'Om barnet' steget
    
    * Legger til bruk av VIS_GUIDE_I_STEG toggle for å fjerne "lesNoeye" tekst når toggle for nye guider er på

--- src/frontend/components/SøknadsSteg/OmBarnet/innholdTyper.ts
+++ src/frontend/components/SøknadsSteg/OmBarnet/innholdTyper.ts
@@ -3,6 +3,7 @@ import { ISanitySpørsmålDokument } from '../../../typer/sanity/sanity';
 
 export interface IOmBarnetTekstinnhold {
     omBarnetTittel: LocaleRecordBlock;
+    omBarnetGuide: LocaleRecordBlock;
     omBarnetTittelUtenFlettefelt: LocaleRecordBlock;
     borBarnFastSammenMedDeg: ISanitySpørsmålDokument;
     paagaaendeSoeknadYtelse: ISanitySpørsmålDokument;
