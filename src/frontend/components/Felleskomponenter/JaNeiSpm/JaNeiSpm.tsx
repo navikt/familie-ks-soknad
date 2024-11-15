a6a76baab371af0927b61ee1688cdc39ca374183
Henning Solberg <henning.solberg@nav.no>
2024-11-15 13:06:49 +0100

    Fjernet logSpørsmålBesvart

--- src/frontend/components/Felleskomponenter/JaNeiSpm/JaNeiSpm.tsx
+++ src/frontend/components/Felleskomponenter/JaNeiSpm/JaNeiSpm.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode } from 'react';
 
 import { v4 as uuidv4 } from 'uuid';
 
@@ -7,11 +7,9 @@ import { ESvar, JaNeiSpørsmål } from '@navikt/familie-form-elements';
 import { Felt, ISkjema } from '@navikt/familie-skjema';
 
 import { useApp } from '../../../context/AppContext';
-import { AlternativtSvarForInput } from '../../../typer/common';
 import { FlettefeltVerdier } from '../../../typer/kontrakt/generelle';
 import { ISanitySpørsmålDokument } from '../../../typer/sanity/sanity';
 import { SkjemaFeltTyper } from '../../../typer/skjema';
-import { logSpørsmålBesvart } from '../../../utils/amplitude';
 import TekstBlock from '../TekstBlock';
 
 interface IJaNeiSpmProps {
@@ -31,20 +29,8 @@ const JaNeiSpm: React.FC<IJaNeiSpmProps> = ({
     spørsmålDokument,
     flettefelter,
 }) => {
-    const [mounted, settMounted] = useState(false);
     const { tekster, plainTekst } = useApp();
     const { ja, nei, jegVetIkke } = tekster().FELLES.frittståendeOrd;
-
-    useEffect(() => {
-        if (mounted) {
-            spørsmålDokument &&
-                logSpørsmålBesvart(
-                    spørsmålDokument.api_navn,
-                    felt.verdi ?? AlternativtSvarForInput.UKJENT
-                );
-        }
-        settMounted(true);
-    }, [felt.verdi]);
 
     return felt.erSynlig ? (
         <div id={felt.id} data-testid={felt.id}>
