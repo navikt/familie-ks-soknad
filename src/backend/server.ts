c759bee5754b8bb505932bc4b5f484e98b8a652f
Henning Solberg <henning.solberg@nav.no>
2025-01-06 13:31:25 +0100

    riktig kommentar

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -39,7 +39,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minutes
-    limit: 200, // Limit each IP to 100 requests per `window` (here, per 5 minutes).
+    limit: 200, // Limit each IP to 200 requests per `window` (here, per 5 minutes).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     // store: ... , // Kan sette opp redis hvis vi ønsker å dele rate-limit over flere instanser
