8f441f7bd106e25e04b2fbf2c9dcaa8209298dfe
Ole Kristian Rudjord <89088327+ole-kristian-rudjord@users.noreply.github.com>
2025-05-12 08:23:53 +0200

    Fikser hardkodet 'og' tekst i slåSammen funksjonen ved å bytte til Sanity tekst (#910)

--- src/frontend/components/Felleskomponenter/VedleggOppsummering/vedleggOppsummering.domene.ts
+++ src/frontend/components/Felleskomponenter/VedleggOppsummering/vedleggOppsummering.domene.ts
@@ -1,3 +1,5 @@
+import { PlainTekst } from '../../../typer/kontrakt/generelle';
+import { IFrittståendeOrdTekstinnhold } from '../../../typer/sanity/tekstInnhold';
 import { slåSammen } from '../../../utils/slåSammen';
 
 import { IVedleggOppsummering } from './vedleggOppsummering.types';
@@ -12,12 +14,21 @@ export const skalVedleggOppsummeringVises = (vedlegg: IVedleggOppsummering[]): b
     return vedlegg.filter(enkeltVedlegg => enkeltVedlegg.skalVises).length > 0;
 };
 
-export const hentVedleggOppsummering = (dokumentasjoner, søknad): IVedleggOppsummering[] =>
+export const hentVedleggOppsummering = (
+    dokumentasjoner,
+    søknad,
+    plainTekst: PlainTekst,
+    frittståendeOrdTekster: IFrittståendeOrdTekstinnhold
+): IVedleggOppsummering[] =>
     dokumentasjoner.map(dokumentasjon => {
         const barnDokGjelderFor = søknad.barnInkludertISøknaden.filter(barn =>
             dokumentasjon.gjelderForBarnId.find(id => id === barn.id)
         );
-        const barnasNavn = slåSammen(barnDokGjelderFor.map(barn => barn.navn));
+        const barnasNavn = slåSammen(
+            barnDokGjelderFor.map(barn => barn.navn),
+            plainTekst,
+            frittståendeOrdTekster
+        );
 
         return {
             skalVises: true,
