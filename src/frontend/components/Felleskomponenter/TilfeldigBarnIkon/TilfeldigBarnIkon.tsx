2174dcbed949ee951cc7b3698baf30d6c1421354
Hanne Olsen <hanne.olsen@nav.no>
2025-05-20 15:53:25 +0200

    Fikser typefeil etter oppgradering

--- src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.tsx
+++ src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.tsx
@@ -8,7 +8,7 @@ import { randomIntFraIntervall } from '../../../utils/hjelpefunksjoner';
 export const TilfeldigBarnIkon: React.FC<{ byttVedRerender?: boolean }> = ({
     byttVedRerender = true,
 }) => {
-    const ikoner: JSX.Element[] = [<Barn1 />, <Barn2 />, <Barn3 />];
+    const ikoner = [<Barn1 />, <Barn2 />, <Barn3 />];
     // Bruker callback istedenfor direkte verdi slik at vi kun kaller randomIntFraIntervall ved første render
     const [fastsattIkonIndex] = useState<number>(() => randomIntFraIntervall(0, ikoner.length - 1));
 
