14e69045516f3694abf26b1d57bd42d617c096c0
Hanne Olsen <hanne.olsen@nav.no>
2025-07-11 10:20:19 +0200

    Mocker ut dekoratøren

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom/vitest';
 
-import { afterAll, afterEach, beforeAll } from 'vitest';
+import { afterAll, afterEach, beforeAll, vi } from 'vitest';
 
 import { server } from './mocks/node';
 
@@ -11,3 +11,12 @@ afterAll(() => server.close());
 window.scrollTo = () => {
     // Ikke implementert
 };
+
+vi.mock('@navikt/nav-dekoratoren-moduler', () => ({
+    setAvailableLanguages: vi.fn().mockImplementation(() => {
+        return Promise.resolve();
+    }),
+    onLanguageSelect: vi.fn(),
+    getAmplitudeInstance: vi.fn(),
+    getCurrentConsent: vi.fn(),
+}));
