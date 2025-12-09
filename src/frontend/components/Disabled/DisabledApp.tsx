c2adfc5c516920d0a9921b362041ee536f00b597
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2025-12-09 17:35:12 +0100

    Forbedrer DisabledApp tekst og lenker

--- src/frontend/components/Disabled/DisabledApp.tsx
+++ src/frontend/components/Disabled/DisabledApp.tsx
@@ -11,27 +11,26 @@ export function DisabledApp() {
                         <Heading level="1" size="large" spacing>
                             Vi jobber med å forbedre siden
                         </Heading>
-                        <BodyShort spacing>
-                            Denne siden er midlertidig utilgjengelig mens vi legger til nytt innhold og gjør
-                            forbedringer. Takk for tålmodigheten. Prøv gjerne igjen litt senere, eller send oss søknaden
-                            på PDF/papir.
-                        </BodyShort>
                         <BodyShort>
-                            <Link href="https://www.nav.no/start/soknad-kontantstotte">Bruk PDF/papirskjema</Link>
+                            Denne siden er midlertidig utilgjengelig mens vi legger til nytt innhold og gjør
+                            forbedringer. Takk for tålmodigheten. Prøv gjerne igjen litt senere, eller{' '}
+                            <Link href="https://www.nav.no/start/soknad-kontantstotte" inlineText>
+                                bruk skjema for innsending i posten
+                            </Link>
+                            .
                         </BodyShort>
                     </div>
                     <div>
                         <Heading level="1" size="large" spacing>
                             We’re working on this page
                         </Heading>
-                        <BodyShort spacing>
-                            This page is temporarily unavailable while we add new content and improvements. Thank you
-                            for your patience. Please check back a bit later, or send the application on PDF/paper.
-                        </BodyShort>
                         <BodyShort>
-                            <Link href="https://www.nav.no/start/soknad-kontantstotte/en">
-                                Send the application on PDF/paper
+                            This page is temporarily unavailable while we add new content and improvements. Thank you
+                            for your patience. Please check back a little later, or you can use{' '}
+                            <Link href="https://www.nav.no/start/soknad-kontantstotte/en" inlineText>
+                                the application form for submission by post
                             </Link>
+                            .
                         </BodyShort>
                     </div>
                 </VStack>
