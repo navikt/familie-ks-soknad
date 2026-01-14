99eba5a16ffe69a1c66344cf2ee7b860718e80bd
ole-kristian-rudjord <olekristianrudjord@gmail.com>
2026-01-14 11:38:22 +0100

    Fikser a11y feil fra feil bruk av dt og dd i periodeoppsummering

--- src/frontend/components/Felleskomponenter/PeriodeOppsummering/PeriodeOppsummering.tsx
+++ src/frontend/components/Felleskomponenter/PeriodeOppsummering/PeriodeOppsummering.tsx
@@ -3,6 +3,7 @@ import React, { ReactNode } from 'react';
 import { TrashFillIcon } from '@navikt/aksel-icons';
 import { Button, FormSummary } from '@navikt/ds-react';
 
+import { useAppContext } from '../../../context/AppContext';
 import { LocaleRecordBlock } from '../../../typer/common';
 import TekstBlock from '../TekstBlock';
 
@@ -14,21 +15,27 @@ interface Props {
 }
 
 function PeriodeOppsummering({ fjernPeriodeCallback = undefined, fjernKnappTekst, tittel, children }: Props) {
+    const { plainTekst } = useAppContext();
+
     return (
         <FormSummary.Answer>
             <FormSummary.Label>{tittel}</FormSummary.Label>
             <FormSummary.Value>
                 <FormSummary.Answers>
                     {children}
-                    {fjernPeriodeCallback !== undefined && (
-                        <Button
-                            type={'button'}
-                            variant={'tertiary'}
-                            onClick={() => fjernPeriodeCallback()}
-                            icon={<TrashFillIcon aria-hidden />}
-                        >
-                            {<TekstBlock block={fjernKnappTekst} />}
-                        </Button>
+                    {fjernPeriodeCallback && fjernKnappTekst && (
+                        <FormSummary.Answer>
+                            <FormSummary.Label hidden>{plainTekst(fjernKnappTekst)}</FormSummary.Label>
+                            <FormSummary.Value>
+                                <Button
+                                    variant="tertiary"
+                                    onClick={fjernPeriodeCallback}
+                                    icon={<TrashFillIcon aria-hidden />}
+                                >
+                                    <TekstBlock block={fjernKnappTekst} />
+                                </Button>
+                            </FormSummary.Value>
+                        </FormSummary.Answer>
                     )}
                 </FormSummary.Answers>
             </FormSummary.Value>
