6f6f0d4085ac6c06a9dc75e5dca92080c0cd6caf
Gard Opsahl Skaare <gard.opsahl.skaare@nav.no>
2026-07-28 14:53:04 +0200

    Ikke kompiler backend-testene til dist

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,8 +7,5 @@ export default defineConfig({
         globals: true,
         environment: 'jsdom',
         setupFiles: './vitest.setup.ts',
-        // Backend kompileres til dist med tsc, og uten denne ville de kompilerte
-        // testfilene i dist blitt kjørt i tillegg til kildefilene i src.
-        include: ['src/**/*.test.{ts,tsx}'],
     },
 });
