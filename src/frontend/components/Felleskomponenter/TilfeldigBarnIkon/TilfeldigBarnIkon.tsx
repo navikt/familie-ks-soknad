1d111e17b2221eb969022b0d3a7a6a73a82827cf
thoalm <thoalm@outlook.com>
2026-08-13 12:48:34 +0200

    Fikser lint/correctness/useJsxKeyInIterable

--- src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.tsx
+++ src/frontend/components/Felleskomponenter/TilfeldigBarnIkon/TilfeldigBarnIkon.tsx
@@ -7,7 +7,7 @@ import Barn3 from '../../../assets/Barn3';
 import { randomIntFraIntervall } from '../../../utils/hjelpefunksjoner';
 
 export const TilfeldigBarnIkon: React.FC<{ byttVedRerender?: boolean }> = ({ byttVedRerender = true }) => {
-    const ikoner = [<Barn1 />, <Barn2 />, <Barn3 />];
+    const ikoner = [<Barn1 key={1} />, <Barn2 key={2} />, <Barn3 key={3} />];
     // Bruker callback istedenfor direkte verdi slik at vi kun kaller randomIntFraIntervall ved første render
     const [fastsattIkonIndex] = useState<number>(() => randomIntFraIntervall(0, ikoner.length - 1));
 
